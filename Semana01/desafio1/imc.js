b3d29420048393cc206b61981ad71eba23368c37
Jessé Ávila <45925514+JesseAvilaa@users.noreply.github.com>
2020-06-20 15:30:30 -0300

    LaunchBase

--- Semana01/desafio1/imc.js
+++ Semana01/desafio1/imc.js
@@ -8,7 +8,7 @@ const sexo = 'Masculino'
 const imc = peso / (altura * altura);
 
 // Se o imc maior ou igual a 30: Jessé você está acima do peso:
-if ( imc >= 30 ) {
+if ( imc >= 60 ) {
     console.log(`${nome} você está acima do peso`)
 }
 
